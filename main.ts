127a5890e661fd2fcdd03c7c3ebc26f8dac08d14
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2022-11-28 23:09:23 +0200

    Try to add another user

--- main.ts
+++ main.ts
@@ -2,42 +2,42 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
-import TemperatureSensors from "./model/testDB.ts";
+// import TemperatureSensors from "./model/testDB.ts";
 
-import {
-    Bson,
-    MongoClient,
-} from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-
-const client = new MongoClient();
+// import {
+//     Bson,
+//     MongoClient,
+// } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+//
+// const client = new MongoClient();
 
 // Connecting to a Local Database
 // await client.connect("mongodb://127.0.0.1:27017");
 
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
 
-// Connecting to a Mongo Atlas Database
-await client.connect({
-    db: "myFirstDatabase",
-    tls: true,
-    servers: [
-        {
-            host: "ac-ie3oda4-shard-00-01.fawqedw.mongodb.net",
-            port: 27017,
-        },
-    ],
-    credential: {
-        username: "Kalaitzo",
-        password: "basilisg4",
-        db: "myFirstDatabase",
-        mechanism: "SCRAM-SHA-1",
-    },
-});
-
-const db = client.database("myFirstDatabase");
-const users = db.collection<TemperatureSensors>("users");
-
-const insertId = await users.insertOne({
-    username: "user2",
-    password: "pass2",
-});
+// // Connecting to a Mongo Atlas Database
+// await client.connect({
+//     db: "myFirstDatabase",
+//     tls: true,
+//     servers: [
+//         {
+//             host: "ac-ie3oda4-shard-00-01.fawqedw.mongodb.net",
+//             port: 27017,
+//         },
+//     ],
+//     credential: {
+//         username: "Kalaitzo",
+//         password: "basilisg4",
+//         db: "myFirstDatabase",
+//         mechanism: "SCRAM-SHA-1",
+//     },
+// });
+//
+// const db = client.database("myFirstDatabase");
+// const users = db.collection<TemperatureSensors>("users");
+//
+// const insertId = await users.insertOne({
+//     username: "user2",
+//     password: "pass2",
+// });
