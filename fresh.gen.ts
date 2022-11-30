69a3619bded6f9199d3f380bc93eb512ae50a268
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2022-11-30 15:03:35 +0200

    Find the users with post on /devices

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,14 +8,14 @@ import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/api/random-uuid.ts";
 import * as $4 from "./routes/countdown.tsx";
-import * as $5 from "./routes/github/[username].tsx";
-import * as $6 from "./routes/greet/[name].tsx";
-import * as $7 from "./routes/index.tsx";
-import * as $8 from "./routes/search.tsx";
-import * as $9 from "./routes/test-header.tsx";
+import * as $5 from "./routes/devices.tsx";
+import * as $6 from "./routes/github/[username].tsx";
+import * as $7 from "./routes/greet/[name].tsx";
+import * as $8 from "./routes/index.tsx";
+import * as $9 from "./routes/search.tsx";
+import * as $10 from "./routes/test-header.tsx";
 import * as $$0 from "./islands/Countdown.tsx";
 import * as $$1 from "./islands/Counter.tsx";
-import * as $$2 from "./routes/devices.tsx";
 
 const manifest = {
   routes: {
@@ -24,12 +24,12 @@ const manifest = {
     "./routes/api/joke.ts": $2,
     "./routes/api/random-uuid.ts": $3,
     "./routes/countdown.tsx": $4,
-    "./routes/github/[username].tsx": $5,
-    "./routes/greet/[name].tsx": $6,
-    "./routes/index.tsx": $7,
-    "./routes/search.tsx": $8,
-    "./routes/test-header.tsx": $9,
-    "./routes/devices.tsx": $$2
+    "./routes/devices.tsx": $5,
+    "./routes/github/[username].tsx": $6,
+    "./routes/greet/[name].tsx": $7,
+    "./routes/index.tsx": $8,
+    "./routes/search.tsx": $9,
+    "./routes/test-header.tsx": $10,
   },
   islands: {
     "./islands/Countdown.tsx": $$0,
--- routes/devices.tsx
+++ routes/devices.tsx
@@ -1,22 +1,63 @@
 // routes/devices.tsx
-import { Handlers } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { config } from "https://deno.land/std/dotenv/mod.ts";
+import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+import TemperatureSensors from "../model/testDB.ts";
 
 export const handler: Handlers = {
     async GET(req, ctx){
         const resp = await ctx.render();
-        resp.headers.set("X-Custom-Header", "Hello");
+        resp.headers.set("X-Custom-Header", "Created from get!");
         return resp;
     },
     async POST(req, ctx) {
-        return(console.log('A post request was made'))
+        const client = new MongoClient();
+
+        const configData = await config();
+        const password = configData["ATLAS_PASS"]
+        const user = configData["ATLAS_USER"]
+        const database = configData["DB"]
+        const host = configData["HOST"]
+
+        // // Connecting to a Mongo Atlas Database
+        await client.connect({
+            db: database,
+            tls: true,
+            servers: [
+                {
+                    host: host,
+                    port: 27017,
+                },
+            ],
+            credential: {
+                username: user,
+                password: password,
+                db: database,
+                mechanism: "SCRAM-SHA-1",
+            }
+        });
+
+        const db = client.database(database);
+        const users = db.collection<TemperatureSensors>("users");
+
+        const all_users = await users.find({ username: { $ne: 'null' } }).toArray();
+        return ctx.render(all_users);
       }
 };
 
-export default function AboutPage(){
+export default function DevicesPage({ data }: PageProps<TemperatureSensors[] | null>){
     return(
         <main>
             <h1>Devices</h1>
-            <p>This is the devices page.</p>
+            {/*{data? <p>{data.username} was found in the database</p> : <p>Didn't search for user one because the GET handler was used</p>}*/}
+            <ul>
+                {data? (data.map((d) => <li>Username: {d.username} and Password: {d.password}</li>)): ('No users')}
+            </ul>
+            <form method={'post'}>
+                <div>
+                    <button type={'submit'} className={"bg-blue-600 text-white rounded px-6 py-2.5"}>Search with POST</button>
+                </div>
+            </form>
         </main>
     );
 }
