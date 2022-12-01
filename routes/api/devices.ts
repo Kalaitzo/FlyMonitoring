c217c00acf31095ce5f12f999e9c14671ec07ad0
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2022-12-01 14:33:57 +0200

    Find the users with post on /devices

--- routes/api/devices.ts
+++ routes/api/devices.ts
@@ -1,15 +1,18 @@
-// routes/api/de.ts
+// routes/api/devices.ts
+
 import { Handlers } from "$fresh/server.ts";
 import db from '../../model/mongodb.ts'
 import TestInterface from "../../model/testDB.ts";
 
 export const handler: Handlers = {
     async POST(req){
-        const payload = req.body
+        const payload =await req.body;
 
-        const testSensors = db.collection<TestInterface>('testSensors')
+        console.log(req)
 
-        const insertId = await testSensors.insertOne(payload);
+        // const testSensors = db.collection<TestInterface>('testSensors')
+
+        // const insertId = await testSensors.insertOne(formData);
 
         return new Response(payload)
     }
