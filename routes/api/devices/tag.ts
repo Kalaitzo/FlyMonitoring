caee6e942c60d625d0f180907e09776640ac496e
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2022-12-08 15:18:47 +0200

    Tag Sensor Handling

---
+++ routes/api/devices/tag.ts
@@ -0,0 +1,34 @@
+// routes/api/devices/tag.ts
+
+import { Handlers } from '$fresh/server.ts';
+import db from "../../../model/mongodb.ts";
+import TagSensor from "../../../model/TagSensor.ts";
+
+export const handler: Handlers = {
+    async POST(req: Request){
+        const payload = req.body;
+
+        // Check if the payload is sent is null
+        if (payload === null) {
+            return new Response("Invalid request: Body is null!");
+        }
+
+        // Create a decoder to decode the data
+        const decoder =  new TextDecoder()
+
+        for await (const chunk of payload){
+            // Decode the bytes into a string then parse it as a JSON
+            const data: TagSensor = JSON.parse(decoder.decode(chunk))
+
+            // Open the collection to store the data that the tag sent
+            const tagSensor = db.collection<TagSensor>('TagSensor')
+
+            // Store the data
+            const insertId = await tagSensor.insertOne(data)
+
+            console.log('Inserted data with id: ' + insertId)
+        }
+
+        return new Response('Got the data from the tag!')
+    }
+}
