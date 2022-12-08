a5e7fb75feabc15ccdfc929c60676fb25801d990
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2022-12-08 18:51:39 +0200

    Added the endpoints to fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,15 +5,18 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[testName].tsx";
 import * as $1 from "./routes/about.tsx";
-import * as $2 from "./routes/api/devices/rack-temperature.ts";
-import * as $3 from "./routes/api/devices/temperature.ts";
-import * as $4 from "./routes/countdown.tsx";
-import * as $5 from "./routes/devices.tsx";
-import * as $6 from "./routes/github/[username].tsx";
-import * as $7 from "./routes/greet/[name].tsx";
-import * as $8 from "./routes/index.tsx";
-import * as $9 from "./routes/search.tsx";
-import * as $10 from "./routes/test-header.tsx";
+import * as $2 from "./routes/api/devices/fluid-level.ts";
+import * as $3 from "./routes/api/devices/rack-temperature.ts";
+import * as $4 from "./routes/api/devices/smoke.ts";
+import * as $5 from "./routes/api/devices/tag.ts";
+import * as $6 from "./routes/api/devices/temperature.ts";
+import * as $7 from "./routes/countdown.tsx";
+import * as $8 from "./routes/devices.tsx";
+import * as $9 from "./routes/github/[username].tsx";
+import * as $10 from "./routes/greet/[name].tsx";
+import * as $11 from "./routes/index.tsx";
+import * as $12 from "./routes/search.tsx";
+import * as $13 from "./routes/test-header.tsx";
 import * as $$0 from "./islands/Countdown.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 
@@ -21,15 +24,18 @@ const manifest = {
   routes: {
     "./routes/[testName].tsx": $0,
     "./routes/about.tsx": $1,
-    "./routes/api/devices/rack-temperature.ts": $2,
-    "./routes/api/devices/temperature.ts": $3,
-    "./routes/countdown.tsx": $4,
-    "./routes/devices.tsx": $5,
-    "./routes/github/[username].tsx": $6,
-    "./routes/greet/[name].tsx": $7,
-    "./routes/index.tsx": $8,
-    "./routes/search.tsx": $9,
-    "./routes/test-header.tsx": $10,
+    "./routes/api/devices/fluid-level.ts": $2,
+    "./routes/api/devices/rack-temperature.ts": $3,
+    "./routes/api/devices/smoke.ts": $4,
+    "./routes/api/devices/tag.ts": $5,
+    "./routes/api/devices/temperature.ts": $6,
+    "./routes/countdown.tsx": $7,
+    "./routes/devices.tsx": $8,
+    "./routes/github/[username].tsx": $9,
+    "./routes/greet/[name].tsx": $10,
+    "./routes/index.tsx": $11,
+    "./routes/search.tsx": $12,
+    "./routes/test-header.tsx": $13,
   },
   islands: {
     "./islands/Countdown.tsx": $$0,
