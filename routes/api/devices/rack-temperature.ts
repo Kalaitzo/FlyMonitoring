f9c82f399a5575a2985a5c6f8240c0f4d8e1ffa0
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2022-12-08 14:09:28 +0200

    Rack Temperature sensors API

---
+++ routes/api/devices/rack-temperature.ts
@@ -0,0 +1,34 @@
+// routes/api/devices/rack-temperature.ts
+
+import { Handlers } from "$fresh/server.ts";
+import db from '../../../model/mongodb.ts'
+import RackTemperatureSensors from "../../../model/RackTemperatureSensors.ts";
+
+export const handler: Handlers = {
+    async POST(req: Request){
+        const payload = req.body;
+
+        // Check if the payload sent is null
+        if (payload === null) {
+            return new Response("Invalid request: Body is null!")
+        }
+
+        // Create a decoder to decode the data
+        const decoder =  new TextDecoder()
+
+        for await (const chunk of payload) {
+            // Decode the bytes into a string and then parse it as a JSON
+            const data: RackTemperatureSensors = JSON.parse(decoder.decode(chunk))
+
+            // Open the collection to store the data that the  temperature sensor sent
+            const rackTemperaturesSensors = db.collection<RackTemperatureSensors>("RackTemperatureSensors")
+
+            // Store the data
+            const insertId = await rackTemperaturesSensors.insertOne(data);
+
+            console.log('Inserted data tih id: ' + insertId);
+        }
+
+        return new Response('Got the data from the rack-temperature sensor!')
+    }
+};
