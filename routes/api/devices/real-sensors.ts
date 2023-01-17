84d31b762e054a17b538a936a1079bca4d327838
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2023-01-17 20:10:16 +0200

    real sensors endpoint

--- routes/api/devices/real-sensors.ts
+++ routes/api/devices/real-sensors.ts
@@ -5,31 +5,31 @@ import { Handlers } from "$fresh/server.ts";
 // import RackTemperatureSensors from "../../../model/schemas/RackTemperatureSensors.ts";
 
 export const handler: Handlers = {
-     POST(req: Request){
+     async POST(req: Request){
         const payload = req.body;
 
-        // // Check if the payload sent is null
-        // if (payload === null) {
-        //     return new Response("Invalid request: Body is null!")
-        // }
-        //
-        // // Create a decoder to decode the data
-        // const decoder =  new TextDecoder()
-        //
-        // for await (const chunk of payload) {
-        //     // Decode the bytes into a string and then parse it as a JSON
-        //     const data: RackTemperatureSensors = JSON.parse(decoder.decode(chunk))
-        //
-        //     // Open the collection to store the data that the rack temperature sensor sent
-        //     const rackTemperaturesSensors = db.collection<RackTemperatureSensors>("RackTemperatureSensors")
-        //
-        //     // Store the data
-        //     const insertId = await rackTemperaturesSensors.insertOne(data);
-        //
-        //     console.log('Inserted data tih id: ' + insertId);
-        // }
-        console.log(payload)
+        // Check if the payload sent is null
+        if (payload === null) {
+            return new Response("Invalid request: Body is null!")
+        }
 
-        return new Response(payload)
+        // Create a decoder to decode the data
+        const decoder =  new TextDecoder()
+
+        for await (const chunk of payload) {
+
+            const sensorPayload = decoder.decode((chunk))
+            // // Decode the bytes into a string and then parse it as a JSON
+            // const data: RackTemperatureSensors = JSON.parse(decoder.decode(chunk))
+            //
+            // // Open the collection to store the data that the rack temperature sensor sent
+            // const rackTemperaturesSensors = db.collection<RackTemperatureSensors>("RackTemperatureSensors")
+            //
+            // // Store the data
+            // const insertId = await rackTemperaturesSensors.insertOne(data);
+
+            console.log('Data' + sensorPayload);
+        }
+        return new Response('I got the post request from the real sensors')
     }
 };
