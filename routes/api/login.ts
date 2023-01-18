077b2a5ab7b26d56bee4e2016837208d2fa8f3f6
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2023-01-18 13:05:25 +0200

    Changed it again so I can use it locally MUST be changed at the final commit to use the database credentials

--- routes/api/login.ts
+++ routes/api/login.ts
@@ -2,13 +2,13 @@
 
 import { Handlers } from "$fresh/server.ts"
 import { setCookie } from "std/http/cookie.ts";
-import db from "../../model/mongodb.ts";
-import Users from "../../model/schemas/Users.ts";
+// import db from "../../model/mongodb.ts";
+// import Users from "../../model/schemas/Users.ts";
 
-// const USER = {
-//     username: 'admin',
-//     password: 'pass'
-// }
+const USER = {
+    username: 'admin',
+    password: 'pass'
+}
 
 export const handler: Handlers = {
     async POST(req){
@@ -16,20 +16,20 @@ export const handler: Handlers = {
         const form = await req.formData();
         const username = form.get('username');
         const password = form.get('password');
-        let user;
+        // let user;
 
         // Open the collection to store the data that the door sensor sent
-        const users = db.collection<Users>("users")
+        // const users = db.collection<Users>("users")
+        // if(username && password){
+        //     user = await users.findOne({
+        //         username: username.toString(),
+        //         password: password.toString(),
+        //     });
+        // }
+        // console.log(user)
 
-        if(username && password){
-            user = await users.findOne({
-                username: username.toString(),
-                password: password.toString(),
-            });
-        }
-        console.log(user)
-        // if(username === USER.username && password === USER.password){
-        if(user){
+        if(username === USER.username && password === USER.password){
+        // if(user){
             const headers = new Headers();
             setCookie(headers, {
                 name: "auth",
