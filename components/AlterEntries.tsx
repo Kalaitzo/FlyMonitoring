5c937fd02eef14df3f852889446277c3d9caf114
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2023-01-27 17:13:43 +0200

    Fixed entries panel when there are no entries detected

---
+++ components/AlterEntries.tsx
@@ -0,0 +1,8 @@
+export default function AlertEntries() {
+    return(
+        <div className={"bg-[#28374F] rounded-lg py-5 px-6 mb-4 text-base text-gray-100 mb-3"} role="alert">
+            No recent activity detected!
+        </div>
+    );
+}
+
--- components/EntiresPanel.tsx
+++ components/EntiresPanel.tsx
@@ -1,4 +1,5 @@
 import {asset} from "$fresh/src/runtime/utils.ts";
+import AlertEntries from "./AlterEntries.tsx";
 
 type data = {
     entries: Array<Record<any, any>>
@@ -6,19 +7,24 @@ type data = {
 
 export default function EntriesPanel({ entries }: data){
     return(
-        <div className={ 'flex flex-col w-max h-max bg-[#28374F] p-7 rounded-lg text-gray-50 justify-center gap-6' }>
-            {entries.map((item) => (
-                <div className = { 'flex flex-row' }>
-                    <img src={ asset('/entrance.png') }
-                         alt={ 'Entrance Icon' }
-                         className={ 'w-12 h-12 self-center'}/>
-                    <div className = { 'self-center pl-3 text-lg' }>
-                        Door Opened:
-                        <br/>
-                        {item.dateLastValueReported}
+        <>
+            {entries.length > 0
+                ? <div className={ 'flex flex-col w-max h-max bg-[#28374F] p-7 rounded-lg text-gray-50 justify-center gap-6' }>
+                    {entries.map((item) => (
+                        <div className = { 'flex flex-row' }>
+                            <img src={ asset('/entrance.png') }
+                                 alt={ 'Entrance Icon' }
+                                 className={ 'w-12 h-12 self-center'}/>
+                            <div className = { 'self-center pl-3 text-lg' }>
+                                Door Opened:
+                                <br/>
+                                {item.dateLastValueReported}
+                            </div>
+                        </div>
+                    ))}
                     </div>
-                </div>
-            ))}
-        </div>
+                : <AlertEntries/>
+            }
+        </>
     )
 }
