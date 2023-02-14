25ed4204d0eb219a811a71120cf384a815f45431
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2023-02-14 11:07:39 +0200

    First attempt to send email

--- routes/api/alert-email.ts
+++ routes/api/alert-email.ts
@@ -1,7 +1,8 @@
 // routes/api/alert-email.ts
 import { Handlers } from "$fresh/server.ts";
-import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
+// import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
 // import { config } from "https://deno.land/std@0.167.0/dotenv/mod.ts";
+import Mailgun from "https://deno.land/x/mailgun@v1.1.0/index.ts";
 import db from "../../model/mongodb.ts"
 
 async function getDataFromSensor(collectionName: string) {
@@ -16,31 +17,44 @@ export const handler: Handlers = {
         // const tempHum = getDataFromSensor('TemperatureSensor')
         // const fluid = getDataFromSensor('FluidLevelSensor')
         // const smoke = getDataFromSensor('SmokeSensor')
-        // const configData = await config()
 
-        // const email = configData['EMAIL']
-        // const password = configData['PWD']
+        const apiKey = Deno.env.get("API_KEY_MAILGUN")
+        const emailSend = Deno.env.get("EMAIL")
+        const emailRecv = Deno.env.get('RECV_EMAIL')
+        const domain = Deno.env.get('DOMAIN_MAILGUN')
 
-        const password = Deno.env.get("PWD")
-        const email = Deno.env.get("EMAIL")
+        //
+        // const client = new SmtpClient();
+        //
+        // await client.connectTLS({
+        //     hostname: "smtp.gmail.com",
+        //     port: 465,
+        //     username: email,
+        //     password: password,
+        // });
+        //
+        // await client.send({
+        //     from: email, // Your Email address
+        //     to: "monitoringfly@gmail.com", // Email address of the destination
+        //     subject: "Mail Title",
+        //     content: "Mail Content ",
+        // });
+        //
+        // await client.close();
 
-        const client = new SmtpClient();
-
-        await client.connectTLS({
-            hostname: "smtp.gmail.com",
-            port: 465,
-            username: email,
-            password: password,
+        // Create a instance using your Mailgun API key and domain
+        const mailgun = new Mailgun({
+            key: apiKey,
+            domain: domain,
         });
 
-        await client.send({
-            from: email, // Your Email address
-            to: "monitoringfly@gmail.com", // Email address of the destination
-            subject: "Mail Title",
-            content: "Mail Content ",
+        // Send your message off to Mailgun!
+        await mailgun.send({
+            to: emailRecv,
+            from: emailSend,
+            text: "How are you doing my friend?",
+            subject: "Just checking up for the second time today!",
         });
-
-        await client.close();
 
         return new Response('Ta kataferame!')
     }
